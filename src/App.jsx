b23e54127795b1cba4861c454932387024bdea50
Roweshil <x-vdm@hotmail.com>
2026-03-29 23:57:00 -0600

    Corregido error critico de render, se ajustan rutas con Link y agregan animaciones simples

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
-import { Header } from './components/Header.jsx';
-import { Footer } from './components/Footer.jsx';
-import { Dashboard } from "./pages/Dashboard.jsx";
+import { Header } from './components/Header.jsx'
+import { Footer } from './components/Footer.jsx'
+import { Dashboard } from "./pages/Dashboard.jsx"
+
 
 
 
