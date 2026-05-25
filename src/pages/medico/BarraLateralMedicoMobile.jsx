b793a08f2bd7fa19229bd83f4a95d75bd32da815
Roweshil <x-vdm@hotmail.com>
2026-05-25 11:57:47 -0600

    Hotfix navbar mobile

--- src/pages/medico/BarraLateralMedicoMobile.jsx
+++ src/pages/medico/BarraLateralMedicoMobile.jsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom"
 
 export function BarraLateralMedicoMobile() {
     return (
-        <aside className="medico-navbar">
-            <h2 className="medico-navbar_title">Menú</h2>
+        <aside className="navbar">
+            <h2 className="navbar_title">Menú</h2>
             <nav>
-                <ul className="medico-navbar__btns">
-                    <li className="medico-navbar__btn"><NavLink to="/" className="bar-link">Home</NavLink></li>
-                    <li className="medico-navbar__btn"><NavLink to="/lista" className="bar-link">Consultar</NavLink></li>
-                    <li className="medico-navbar__btn"><NavLink to="/crear" className="bar-link">Crear</NavLink></li>
+                <ul className="navbar__btns">
+                    <li className="navbar__btn"><NavLink to="/" className="bar-link">Home</NavLink></li>
+                    <li className="navbar__btn"><NavLink to="/lista" className="bar-link">Consultar</NavLink></li>
+                    <li className="navbar__btn"><NavLink to="/crear" className="bar-link">Crear</NavLink></li>
                 </ul>
             </nav>
         </aside>
